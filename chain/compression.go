20b3d02cb9e09d62b711762d77fd9a5602deea0e
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-04-16 22:26:38 +0200

    Adding chain/compression.go

---
+++ chain/compression.go
@@ -0,0 +1,37 @@
+package chain
+
+import "github.com/pnx/antelope-go/abi"
+
+type CompressionType uint8
+
+const (
+	CompressionNone = CompressionType(iota)
+	CompressionZlib
+)
+
+func (c CompressionType) String() string {
+	switch c {
+	case CompressionNone:
+		return "none"
+	case CompressionZlib:
+		return "zlib"
+	default:
+		return ""
+	}
+}
+
+// abi.Marshaler conformance
+
+func (b CompressionType) MarshalABI(e *abi.Encoder) error {
+	return e.WriteByte(byte(b))
+}
+
+// abi.Unmarshaler conformance
+
+func (b *CompressionType) UnmarshalABI(d *abi.Decoder) error {
+	var err error
+	if v, err := d.ReadByte(); err == nil {
+		*b = CompressionType(v)
+	}
+	return err
+}
